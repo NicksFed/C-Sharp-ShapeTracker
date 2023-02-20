93c23d75b674c813e4bea0eb7b99b8e5901f935f
Mike <ZackCipolletta@gmail.com>
2023-02-20 09:27:15 -0800

    add _side3 get functionality + passing test

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -10,16 +10,19 @@ namespace ShapeTracker.Models
       get { return _side1; }
       set { _side1 = value; }
     }
-    public Triangle(int length1)
-    {
-      _side1 = length1;
-    }
     public int Side2 { get; set; }
+    private int _side3;
 
-    public Triangle(int length1, int length2)
+    public Triangle(int length1, int length2, int length3)
     {
       _side1 = length1;
       Side2 = length2;
+      _side3 = length3;
+    }
+
+    public int GetSide3()
+    {
+      return _side3;
     }
   }
 }
