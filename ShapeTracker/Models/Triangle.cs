a762f1ace519ac3ab67a0a3cfe0082c25a73d397
Mike <ZackCipolletta@gmail.com>
2023-02-20 10:32:24 -0800

    completed all testing

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,4 +1,4 @@
-// using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace ShapeTracker.Models 
 {
@@ -12,12 +12,14 @@ namespace ShapeTracker.Models
     }
     public int Side2 { get; set; }
     private int _side3;
+    private static List<Triangle> _instances = new List<Triangle> { };
 
     public Triangle(int length1, int length2, int length3)
     {
       _side1 = length1;
       Side2 = length2;
       _side3 = length3;
+      _instances.Add(this);
     }
 
     public int GetSide3()
@@ -32,7 +34,32 @@ namespace ShapeTracker.Models
 
     public string CheckType()
     {
-      return "not a triangle";
+      if ((Side1 > (Side2 + _side3)) || (Side2 > (Side1 + _side3)) || (_side3 > (Side1 + Side2)))
+      {
+        return "not a triangle";
+      }
+      else if ((Side1 != Side2) && ((Side1 != _side3)) && ((Side2 != _side3)))
+      {
+        return "scalene triangle";
+      }
+      else if ((Side1 == Side2) && (Side1 == _side3))
+      {
+        return "equilateral triangle";
+      }
+      else
+      {
+        return "isosceles triangle";
+      }
+    }
+
+    public static List<Triangle> GetAll()
+      {
+        return _instances;
+      }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
